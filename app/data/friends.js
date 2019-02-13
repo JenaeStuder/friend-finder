3fa3693b9ff6a1a76e0c695bbf7185f59d84c7c8
Jenae <Jenaenay15@yahoo.com>
2019-02-12 20:01:31 -0700

    added express static to make css and images work

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,2 +1,173 @@
-let friendArray =[];
+let friendArray =[
+    {
+        "name":"Devin",
+        "photo": "https://images.pexels.com/photos/1722198/pexels-photo-1722198.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        "scores":[
+            "2",
+            "5",
+            "3",
+            "1",
+            "4",
+            "1",
+            "1",
+            "1",
+            "3",
+            "4"
+        ]
+    },
+    {
+        "name":"Cindy",
+        "photo": "https://images.pexels.com/photos/1288171/pexels-photo-1288171.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        "scores":[
+            "5",
+            "5",
+            "5",
+            "5",
+            "1",
+            "1",
+            "2",
+            "4",
+            "4",
+            "3"
+        ]
+
+    },
+    {
+        "name":"Marsha",
+        "photo": "https://images.pexels.com/photos/1669158/pexels-photo-1669158.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        "scores":[
+            "2",
+            "2",
+            "2",
+            "3",
+            "1",
+            "4",
+            "5",
+            "3",
+            "4",
+            "5"
+        ]
+
+    },
+    {
+        "name":"Jason",
+        "photo": "https://images.pexels.com/photos/1445527/pexels-photo-1445527.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        "scores":[
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+
+    },
+    {
+        "name":"Zara",
+        "photo": "https://images.pexels.com/photos/1209797/pexels-photo-1209797.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        "scores":[
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "4",
+            "3",
+            "2",
+            "1",
+            "1"
+        ]
+
+    },
+    {
+        "name":"Pete",
+        "photo": "https://images.pexels.com/photos/1170261/pexels-photo-1170261.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        "scores":[
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]
+
+    },
+    {
+        "name":"Yolanda",
+        "photo": "https://images.pexels.com/photos/1644924/pexels-photo-1644924.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        "scores":[
+            "3",
+            "3",
+            "2",
+            "2",
+            "4",
+            "5",
+            "5",
+            "1",
+            "4",
+            "3"
+        ]
+
+    },
+    {
+        "name":"Bonnie",
+        "photo": "https://images.pexels.com/photos/1624719/pexels-photo-1624719.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        "scores":[
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "3",
+            "2",
+            "1",
+            "4",
+            "3"
+        ]
+
+    },
+    {
+        "name":"Spencer",
+        "photo": "https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        "scores":[
+            "2",
+            "4",
+            "1",
+            "3",
+            "5",
+            "3",
+            "2",
+            "1",
+            "2",
+            "3"
+        ]
+
+    },
+    {
+        "name":"Julio",
+        "photo": "https://images.pexels.com/photos/1706910/pexels-photo-1706910.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        "scores":[
+            "4",
+            "4",
+            "3",
+            "4",
+            "3",
+            "4",
+            "3",
+            "4",
+            "3",
+            "4"
+        ]
+
+    },
+];
 module.exports = friendArray;
+
